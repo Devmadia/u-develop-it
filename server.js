70a78c465825296fa9c83774937a0c396e8fc30e
Devmadia <agathaphan@icloud.com>
2020-08-04 22:06:32 -0500

    front-end team can loop and parse this object on the front end to display a list of the candidates' names

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ const db = new sqlite3.Database('./db/election.db', err => { // created a new ob
     console.log('Connected to the election database.'); // success message will appear if the connection was made correctly
 });
 
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      
+      // If there was no error, then err is null and the response is sent back using the following statement
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
 // // Test the Express.js Connection
 // app.get('/', (req, res) => {
 //     res.json({
@@ -49,16 +67,16 @@ const db = new sqlite3.Database('./db/election.db', err => { // created a new ob
 // });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result, this.lastID);
-});
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
