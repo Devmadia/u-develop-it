0e30b9745466db7f38e82e5c1cf10a1fa0e43838
Devmadia <agathaphan@icloud.com>
2020-08-04 21:57:30 -0500

    fulfilled the requests in a form SQLite can understand

--- server.js
+++ server.js
@@ -33,11 +33,31 @@ const db = new sqlite3.Database('./db/election.db', err => { // created a new ob
 // });
 
 // GET a single candidate
-db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-      console.log(err);
-    }
-    console.log(row);
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
 });
 
 // Default response for any other request(Not Found) Catch all
