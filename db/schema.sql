625eb9fb9be07e4755f9503a4d8be3a9f2a7825c
Devmadia <agathaphan@icloud.com>
2020-08-05 22:26:03 -0500

    updated candidates table to reference parties and to join with party data

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,15 @@
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS candidates;
+
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  industry_connected BOOLEAN NOT NULL,
+  party_id INTEGER UNSIGNED,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
 
 CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
@@ -11,8 +17,3 @@ CREATE TABLE parties (
   description TEXT
 );
 
-INSERT INTO parties (name, description)
-VALUES
-  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
-  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
-  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
