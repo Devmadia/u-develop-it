b7116c2239da7a713cac238a72a462de4199eb4a
Devmadia <agathaphan@icloud.com>
2020-08-05 23:13:39 -0500

    finished migrating party routes to appropriate files, checking API routes successfully retrieved data, no errors

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -47,6 +47,31 @@ router.get('/candidate/:id', (req, res) => {
     });
 });
 
+// Create a candidate
+router.post('/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
 // routes to handle updates
 router.put('/candidate/:id', (req, res) => {
     const sql = `UPDATE candidates SET party_id = ? 
@@ -84,27 +109,4 @@ router.delete('/candidate/:id', (req, res) => {
     });
   });
 
-// Create a candidate
-router.post('/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-      res.status(400).json({ error: errors });
-      return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-              VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-    // ES5 function, not arrow function, to use `this`
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: body,
-            id: this.lastID
-        });
-    });
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,76 +1,21 @@
 const express = require('express');
 const db = require('./db/database');
-const apiRoutes = require('./routes/apiRoutes');
-app.use('/api', apiRoutes);
-
-// imports the inputCheck module
-const inputCheck = require('./utils/inputCheck');
 
 // port designation and add expression
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const apiRoutes = require('./routes/apiRoutes');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// imports the inputCheck module
+// const inputCheck = require('./utils/inputCheck');
 
-
-// route for all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-});
-
-// route that includes an id parameter for a single party
-app.get('/api/party/:id', (req, res) => {
-    const errors = inputCheck(req.body, 'party_id');
-
-    if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-    }
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-});
-
-// a delete route
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        return;
-      }
-  
-      res.json({ message: 'successfully deleted', changes: this.changes });
-    });
-});
-
-
+// use apiRoutes
+app.use('/api', apiRoutes);
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
